78024c6414b4daf6a5a53215ed77c574e5f45f41
hp <aaamab257@gmail.com>
2023-08-06 14:01:10 +0300

    latest version 7

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,10 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
-class UserRegisterForm(forms.ModelForm):
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+
     class Meta:
         model = User
-        fields = ( 'email', 'phone', 'name')
+        fields = ('username', 'email', 'password1', 'password2')
