d83772c985ae84cd1625513f6325da5550f7a18d
hp <aaamab257@gmail.com>
2023-08-07 13:46:53 +0300

    fix validations in auth

--- accounts/models.py
+++ accounts/models.py
@@ -62,7 +62,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         unique=True,
     )
     name = models.CharField(max_length=255)
-    phone = models.BigIntegerField(unique=True,)
+    phone = models.CharField( max_length=20, unique=True,)
     date_of_birth = models.CharField(max_length=55,null=True,blank=False)
     is_vendor=  models.BooleanField(default=False , null=True)
     is_user = models.BooleanField(default=False, null=True)
